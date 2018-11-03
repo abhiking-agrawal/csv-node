880122c77be1af269593403aca0059c1e1303d83
abhi <s530670@nwmissouri.edu>
2018-11-03 13:51:35 -0500

    Added cors package

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const formidable = require('express-formidable');
 var logger = require('morgan');
+var cors = require('cors');
 
 require('./config/database');
 
@@ -20,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(formidable());
+app.use(cors());
 app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
