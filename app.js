7eca175859f6681f1115513969217744ba1a3eff
abhi <s530670@nwmissouri.edu>
2018-11-03 13:54:38 -0500

    Added back the formiddible for testing

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use(formidable());
+
 app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
